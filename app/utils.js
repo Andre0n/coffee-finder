ae0ace2a271fb328a5609dd80e671c87cecb86d6
Andre0n <andreon098@gmail.com>
2024-10-13 19:00:53 -0300

    feat: update `render` function to use EJS

--- app/utils.js
+++ app/utils.js
@@ -1,8 +1,27 @@
+import ejs from 'ejs';
 import { join } from 'node:path';
-import { createReadStream } from 'node:fs';
 
-export const render = (response, view) => {
-  const filePath = join('views', view);
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  return createReadStream(filePath).pipe(response);
+const viewsPath = join(process.cwd(), 'views');
+const options = {
+  root: process.cwd(),
+  views: [viewsPath],
+};
+
+export const render = (response, view, data) => {
+  const filePath = join(viewsPath, view);
+
+  ejs.renderFile(filePath, data, options, function (err, str) {
+
+    if (err) {
+      console.error(err);
+      response.writeHead(500, { 'Content-Type': 'text/plain' });
+      response.write('Internal Server Error');
+      response.end();
+      return;
+    }
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(str);
+    response.end();
+  });
 };

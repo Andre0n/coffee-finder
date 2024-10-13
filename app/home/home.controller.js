19ca10ad69e110b939e5ab1ffee62e041bff2b48
Andre0n <andreon098@gmail.com>
2024-10-13 18:37:58 -0300

    feat: add database initialization endpoint
    - Added logic to initialize the database when the query parameter
    `init=true` is present in development environment.

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,5 +1,19 @@
+import { URL } from 'node:url';
 import { render } from '../utils.js';
+import { initDatabase } from '../database.js';
 
-export const home = (_, response) => {
+export const home = (request, response) => {
+  const { url } = request;
+  const { searchParams } = new URL(url, `http://${request.headers.host}`);
+
+  if (searchParams.has('init')) {
+    if (
+      process.env.NODE_ENV === 'development' &&
+      searchParams.get('init') === 'true'
+    ) {
+      initDatabase();
+    }
+  }
+
   return render(response, 'index.html');
 };

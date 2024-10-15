55d86bf863b144c8493646ab0c96b5e58dad3a4b
Andre0n <andreon098@gmail.com>
2024-10-14 21:03:26 -0300

    feat: update home.controller to use home.service

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,9 +1,6 @@
 import { render } from '../utils.js';
+import { getCoffeShops } from './home.service.js';
 
 export const home = (_, response) => {
-  const data = {
-    page: 'Home',
-  };
-
-  return render(response, 'index.ejs', data);
+  return getCoffeShops(response);
 };
